1929cf743101de14136210c2a34d450935ee334c
Devilicht <oblivionccyber@gmail.com>
2023-05-22 13:28:10 -0400

    feat: added swagger in routes

--- router/routes.go
+++ router/routes.go
@@ -1,16 +1,27 @@
 package router
 
 import (
-	"github.com/Devilicht/oportunities.git/handler"
+	docs "github.com/arthur404dev/gopportunities/docs"
+	"github.com/arthur404dev/gopportunities/handler"
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
-func initializeRoutes(router *gin.Engine){
-	v1 := router.Group("/api/v1")
-	{	v1.GET("/opening", handler.ShowOpeningHandler)
+func initializeRoutes(router *gin.Engine) {
+	// Initialize Handler
+	handler.InitializeHandler()
+	basePath := "/api/v1"
+	docs.SwaggerInfo.BasePath = basePath
+	v1 := router.Group(basePath)
+	{
+		v1.GET("/opening", handler.ShowOpeningHandler)
 		v1.POST("/opening", handler.CreateOpeningHandler)
 		v1.DELETE("/opening", handler.DeleteOpeningHandler)
 		v1.PUT("/opening", handler.UpdateOpeningHandler)
-		v1.GET("/openings", handler.ListOpeningHandler)	
+		v1.GET("/openings", handler.ListOpeningsHandler)
+
 	}
+	// Initialize Swagger
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 }
